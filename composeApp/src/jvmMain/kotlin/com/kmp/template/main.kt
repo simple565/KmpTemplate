89ffe8de8bfdb1bc10dce04ed0de7653653190f4
Simple <467914885@qq.com>
2025-12-06 15:40:32 +0800

    设置桌面端窗口大小

--- composeApp/src/jvmMain/kotlin/com/kmp/template/main.kt
+++ composeApp/src/jvmMain/kotlin/com/kmp/template/main.kt
@@ -1,13 +1,24 @@
 package com.kmp.template
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.kmp.template.ui.App
 
 fun main() = application {
+    val windowState = rememberWindowState(
+        position = WindowPosition.Aligned(alignment = Alignment.Center),
+        size = DpSize(1080.dp, 800.dp)
+    )
+    
     Window(
         onCloseRequest = ::exitApplication,
         title = "KmpTemplate",
+        state = windowState,
     ) {
         App()
     }
